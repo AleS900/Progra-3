a6a8d6fa705dfc7b6c87591c6c2263695dedde90
Santiago <santiavctf@gmail.com>
2020-06-20 17:59:48 -0400

    Pokedex

--- MyPokedexSAVC/MyPokedex/app/src/main/java/bo/upb/programacion3/codelabpokedex/callback/PokemonCallback.java
+++
@@ -1,7 +0,0 @@
-package bo.upb.programacion3.codelabpokedex.callback;
-
-import bo.upb.programacion3.codelabpokedex.model.Pokemon;
-
-public interface PokemonCallback {
-    void onPokemonClick(Pokemon pokemon);
-}
